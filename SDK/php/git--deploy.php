8b7c161f083a9391e77a7bb4ab6fd0caa818cb08
ldco <ldco@pop-os.localdomain>
2020-04-12 01:16:14 +0300

    self commit 2020-04-12 01:16:14

--- SDK/php/git--deploy.php
+++ SDK/php/git--deploy.php
@@ -9,27 +9,30 @@ $now = date("Y-m-d H:i:s");
 
 if (isset($_POST["gitself"])) {
     if (isset($_POST["gitmaster"])) {
-        exec('git add .', $output1, $status1);
-        exec('git commit -m "self commit ' . $now, $output2, $status2);
-        exec('git push -u origin master', $output3, $status3);
-        echo json_encode($output1 . $output1 . $output3);
+        exec('git add .', $output, $status);
+        exec('git commit -m "self commit ' . $now . '"', $output, $status);
+        exec('git push -u origin master', $output, $status);
+        echo json_encode($output . $status);
     } else {
-        exec('git add . &&
-git commit -m "self commit ' . $now . '"
- && git push -u ' . $_POST["gitto"], $output, $status);
-        echo json_encode($output);
+
+        exec('git add .', $output, $status);
+        exec('git commit -m "self commit ' . $now . '"', $output, $status);
+        exec('git push -u ' . $_POST["gitto"], $output, $status);
+        echo json_encode($output . $status);
     }
 } else {
     if (isset($_POST["gitmaster"])) {
-        exec('git add . &&
-git commit -m "' . $_POST["gitcom"] . '"
- && git push -u origin master', $output, $status);
-        echo json_encode($output);
+
+        exec('git add .', $output, $status);
+        exec('git commit -m "self commit ' . $now . '"', $output, $status);
+        exec('git push -u ' . $_POST["gitto"], $output, $status);
+        echo json_encode($output . $status);
     } else {
-        exec('git add . &&
-git commit -m "' . $_POST["gitcom"] . '"
- && git push -u ' . $_POST["gitto"], $output, $status);
-        echo json_encode($output);
+
+        exec('git add .', $output, $status);
+        exec('git commit -m "' . $_POST["gitcom"] . '"', $output, $status);
+        exec('git push -u ' . $_POST["gitto"], $output, $status);
+        echo json_encode($output . $status);
     }
 }
 
