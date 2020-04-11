8b048cd8a9d34d8b990c5874e58246d11628e04b
ldco <ldco@pop-os.localdomain>
2020-04-11 03:50:07 +0300

    fix dist sh script

--- etc/sh/dist-build.sh
+++ etc/sh/dist-build.sh
@@ -7,15 +7,7 @@ rsync -arvh --delete index.php DIST &&
 
 rsync -arvh --delete www/master.min.js www/master-l-prod.min.css www/master-d-prod.min.css www/master-l-prod.css.map www/master-d-prod.css.map DIST/www &&
 
-rsync -arvh --delete sys/Model/ DIST/Model &&
-rsync -arvh --delete sys/View/ DIST/View &&
-rsync -arvh --delete sys/Controller/ DIST/Controller &&
-rsync -arvh --delete sys/helpers/ DIST/helpers &&
-rsync -arvh --delete sys/modules/ DIST/modules &&
-rsync -arvh --delete sys/Pages/ DIST/Pages &&
-rsync -arvh --delete sys/SysInfo/ DIST/SysInfo &&
-rsync -arvh --delete vendor/ DIST/vendor &&
-
+rsync -arvh --delete --exclude 'assets/videos/videos_dev' --exclude 'assets/icons/vector_dev' --exclude 'assets/images/images_dev' --exclude 'JS' --exclude 'sql'  --exclude 'styles' sys/ DIST/sys &&
 
 rsync -arvh --delete --exclude 'videos/videos_dev' --exclude 'icons/vector_dev' --exclude 'images/images_dev' sys/assets/ DIST/assets &&
 
