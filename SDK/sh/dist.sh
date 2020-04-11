9b47e3772734bd8ee8a1a5725c7d99ac67d7a78b
ldco <ldco@pop-os.localdomain>
2020-04-11 20:07:17 +0300

    DEPLOY TO PRODUCTION 2020_04_11__20_07_16

--- SDK/sh/dist.sh
+++ SDK/sh/dist.sh
@@ -3,9 +3,9 @@
 
 rm -rf dist/*
 
-rsync -arvh --delete .htaccess DIST &&
+rsync -arvh --delete .htaccess dist &&
 
-rsync -arvh --delete index.php DIST &&
+rsync -arvh --delete index.php dist &&
 
 rsync -arvh --delete www/master.min.js www/master-l-prod.min.css www/master-d-prod.min.css www/master-l-prod.css.map www/master-d-prod.css.map dist/www &&
 
---
+++ dist/index.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+if (
+    !empty($_SERVER) && !empty($_SERVER['SERVER_SOFTWARE']) && (strpos($_SERVER['SERVER_SOFTWARE'], 'PHP') !== false) &&
+    strpos($_SERVER['SERVER_SOFTWARE'], 'Development') && strpos($_SERVER['SERVER_SOFTWARE'], 'Server')
+) {
+    define("PM_RUN_DEV", true);
+    session_start();
+}
+
+if (file_exists(dirname($_SERVER['DOCUMENT_ROOT'], 1) . "/config.ini.php")) {
+    require dirname($_SERVER['DOCUMENT_ROOT'], 1) . "/config.ini.php";
+} else {
+    throw new Exception("No config file found!");
+}
+
+if (basename($_SERVER['DOCUMENT_ROOT']) === PM_REMOTE_APPFOLDER) {
+    if (PM_IS_DEV) {
+        require_once $_SERVER['DOCUMENT_ROOT'] . "/PM_DEV/" . PM_SYS_FOLDER . "/Model/startup.model.php";
+        require_once $_SERVER['DOCUMENT_ROOT'] . "/PM_DEV/" . PM_SYS_FOLDER . "/Controller/Main.ctrl.php";
+    } else {
+        require_once $_SERVER['DOCUMENT_ROOT'] . "/" . PM_SYS_FOLDER . "/Model/startup.model.php";
+        require_once $_SERVER['DOCUMENT_ROOT'] . "/" . PM_SYS_FOLDER . "/Controller/Main.ctrl.php";
+    }
+} else {
+    require_once $_SERVER['DOCUMENT_ROOT'] . "/" . PM_SYS_FOLDER . "/Model/startup.model.php";
+    require_once $_SERVER['DOCUMENT_ROOT'] . "/" . PM_SYS_FOLDER . "/Controller/Main.ctrl.php";
+}
