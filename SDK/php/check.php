2aac4327b126eee7b566781515c9a7167e8dae46
ldco <fabrica21@yandex.ru>
2020-04-12 01:31:21 +0300

    self commit 2020-04-12 01:31:21

---
+++ SDK/php/check.php
@@ -0,0 +1,50 @@
+<?php
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL); ?>
+
+<?php
+
+
+$now = date("Y-m-d H:i:s");
+
+if (isset($_POST["gitself"])) {
+    if (isset($_POST["gitmaster"])) {
+
+        exec('git add . &&
+        git commit -m "self commit" &&
+        git push -u origin master', $output, $status);
+        echo $output;
+
+
+        /*  exec('git add .', $output, $status);
+        exec('git commit -m "self commit ' . $now, $output, $status);
+        exec('git push -u origin master', $output, $status);
+        echo json_encode($output); */
+    } else {
+
+        exec('git add .', $output, $status);
+        exec('git commit -m "self commit ' . $now, $output, $status);
+        exec('git push -u ' . $_POST["gitto"], $output, $status);
+        echo json_encode($output . $status);
+    }
+} else {
+    if (isset($_POST["gitmaster"])) {
+
+        exec('git add .', $output, $status);
+        exec('git commit -m "self commit ' . $now, $output, $status);
+        exec('git push -u ' . $_POST["gitto"], $output, $status);
+        echo json_encode($output . $status);
+    } else {
+
+        exec('git add .', $output, $status);
+        exec('git commit -m "' . $_POST["gitcom"], $output, $status);
+        exec('git push -u ' . $_POST["gitto"], $output, $status);
+        echo json_encode($output . $status);
+    }
+}
+
+
+
+/* PM_GIT_REPO
+PM_GIT_PASS */
