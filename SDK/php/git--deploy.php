e98c29b333de47be0ce94dca5013257623f00107
ldco <ldco@pop-os.localdomain>
2020-04-12 01:07:13 +0300

    self commit 2020-04-12 01:07:13

--- SDK/php/git--deploy.php
+++ SDK/php/git--deploy.php
@@ -10,9 +10,13 @@ $now = date("Y-m-d H:i:s");
 if (isset($_POST["gitself"])) {
     if (isset($_POST["gitmaster"])) {
 
-        exec('git add . &&
-git commit -m "self commit ' . $now . '"
- && git push -u origin master', $output, $status);
+        exec('git add .', $output, $status);
+        echo json_encode($status);
+
+        exec('git commit -m "self commit ' . $now, $output, $status);
+        echo json_encode($status);
+
+        exec('git push -u origin master', $output, $status);
         echo json_encode($status);
     } else {
         exec('git add . &&
