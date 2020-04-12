a351b266c55d64d380bda3de3ce82e268fd90b0e
ldco <fabrica21@yandex.ru>
2020-04-12 14:03:20 +0300

    self commit 2020-04-12 14:03:20

--- SDK/php/git--deploy.php
+++ SDK/php/git--deploy.php
@@ -12,24 +12,24 @@ if (isset($_POST["gitself"])) {
 
         $com = shell_exec("git commit -am 'self commit " . $now . "'");
         $push = shell_exec("git push -u origin master");
-        echo "1 -> \r\n git commit -am 'self commit " . $now . "'" . "\r\n" . $com . "git push -u origin master" . "\r\n" . $push;
+        echo "1 -> git commit -am 'self commit " . $now . "'" . $com . "git push -u origin master" . $push;
     } else {
 
         $com = shell_exec("git commit -am 'self commit " . $now . "'");
         $push = shell_exec("git push -u" . $_POST['gitto']);
-        echo "2 -> git commit -am 'self commit " . $now . "'" . "\r\n" . $com . "git push -u" . $_POST['gitto']  . "\r\n" . $push;
+        echo "2 -> git commit -am 'self commit " . $now . "'" . $com . "git push -u" . $_POST['gitto']  . $push;
     }
 } else {
     if (isset($_POST["gitmaster"])) {
 
         $com = shell_exec("git commit -am" . $_POST['gitcom']);
         $push = shell_exec("git push -u origin master");
-        echo "3 -> git commit -am 'self commit " . $_POST['gitcom'] . "'" . "\r\n" . $com . "git push -u origin master" . $push;
+        echo "3 -> git commit -am 'self commit " . $_POST['gitcom'] . "'" . $com . "git push -u origin master" . $push;
     } else {
 
         $com = shell_exec("git commit -am" . $_POST['gitcom']);
         $push = shell_exec("git push -u" . $_POST['gitto']);
-        echo "4 -> git commit -am 'self commit " . $_POST['gitcom'] . "'" . "\r\n" . $com . "git push -u" . $_POST['gitto']  . "\r\n" . $push;
+        echo "4 -> git commit -am 'self commit " . $_POST['gitcom'] . "'" . $com . "git push -u" . $_POST['gitto']  . $push;
     }
 }
 
