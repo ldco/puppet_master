dc07fcfafc48fced8233cd0b53986a883f85eb29
ldco <fabrica21@yandex.ru>
2020-04-13 11:33:12 +0300

    self commit 2020-04-13 11:33:12

--- SDK/php/git--deploy.php
+++ SDK/php/git--deploy.php
@@ -7,24 +7,26 @@ error_reporting(E_ALL); ?>
 
 $now = date("Y-m-d H:i:s");
 
+echo $_POST["gitself"] . $_POST["gitmaster"];
+
 if (isset($_POST["gitself"]) && $_POST["gitself"] == true) {
     if (isset($_POST["gitmaster"]) && $_POST["gitmaster"] == true) {
         $com = shell_exec("git commit -am 'self commit " . $now . "'");
         $push = shell_exec("git push -u origin master");
         echo "1 -> git commit -am 'self commit " . $now . "'" . $com . "git push -u origin master" . $push;
-    } else {
+    } else if (isset($_POST["gitmaster"]) && $_POST["gitmaster"] == false) {
 
         $com = shell_exec("git commit -am 'self commit " . $now . "'");
         $push = shell_exec("git push -u" . $_POST['gitto']);
         echo "2 -> git commit -am 'self commit " . $now . "'" . $com . "git push -u" . $_POST['gitto']  . $push;
     }
-} else {
+} else if (isset($_POST["gitself"]) && $_POST["gitself"] == false) {
     if (isset($_POST["gitmaster"]) && $_POST["gitmaster"] == true) {
 
         $com = shell_exec("git commit -am " . $_POST['gitcom']);
         $push = shell_exec("git push -u origin master");
         echo "3 -> git commit -am 'self commit " . $_POST['gitcom'] . "'" . $com . "git push -u origin master" . $push;
-    } else {
+    } else if (isset($_POST["gitmaster"]) && $_POST["gitmaster"] == false) {
 
         $com = shell_exec("git commit -am " . $_POST['gitcom']);
         $push = shell_exec("git push -u" . $_POST['gitto']);
