e10e434dd16f09aff8d59e694d2860d93ab36db8
ldco <fabrica21@yandex.ru>
2020-04-13 17:37:28 +0300

    DEPLOY TO PRODUCTION 2020_04_13 17:37:28

--- SDK/php/execNpm.php
+++ SDK/php/execNpm.php
@@ -2,11 +2,9 @@
 
 
 if (isset($_POST["npm"])) {
-
-
     $el =  strtok($_POST["npm"], " (");
     $start = "starting npm run " . $el . "...";
-    $shell = shell_exec("npm run " . $el);
+    $shell = shell_exec("npm run " . $el . "; echo $?");
     echo  $start;
     if ($shell) {
         echo $shell;
--- SDK/sh/git--self--deploy.sh
+++ SDK/sh/git--self--deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-date=`date '+%Y_%m_%d__%H_%M_%S'`;
+date=`date '+%Y_%m_%d %H:%M:%S'`;
 git add .
 git commit -m "DEPLOY TO PRODUCTION $date"
 git push -u origin master
