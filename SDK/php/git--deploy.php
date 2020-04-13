7be2a0808c81a212b39dbe41fc41ba143b3e3b26
ldco <fabrica21@yandex.ru>
2020-04-13 11:56:44 +0300

    self commit 2020-04-13 11:56:44

--- SDK/php/git--deploy.php
+++ SDK/php/git--deploy.php
@@ -7,29 +7,33 @@ error_reporting(E_ALL); ?>
 
 $now = date("Y-m-d H:i:s");
 
+$com = "Com";
+$push = "Push";
+
+
 echo $_POST["gitself"] . $_POST["gitmaster"];
 
-if ($_POST["gitself"] == true) {
-    if ($_POST["gitmaster"] == true) {
+if ($_POST["gitself"] == "true") {
+    if ($_POST["gitmaster"] == "true") {
         $com = shell_exec("git commit -am 'self commit " . $now . "'");
         $push = shell_exec("git push -u origin master");
-        echo "1 -> git commit -am 'self commit " . $now . "'" . $com . "git push -u origin master" . $push;
-    } else if ($_POST["gitmaster"] == "false") {
+        echo "1 -> git commit -am 'self commit " . $now . "' " . $com . " git push -u origin master " . $push;
+    } else {
 
         $com = shell_exec("git commit -am 'self commit " . $now . "'");
         $push = shell_exec("git push -u" . $_POST['gitto']);
-        echo "2 -> git commit -am 'self commit " . $now . "'" . $com . "git push -u" . $_POST['gitto']  . $push;
+        echo "2 -> git commit -am 'self commit " . $now . "' " . $com . " git push -u " . $_POST['gitto']  . " " . $push;
     }
-} else if ($_POST["gitself"] == "false") {
-    if ($_POST["gitmaster"] == true) {
+} else {
+    if ($_POST["gitmaster"] == "true") {
 
         $com = shell_exec("git commit -am " . $_POST['gitcom']);
         $push = shell_exec("git push -u origin master");
-        echo "3 -> git commit -am 'self commit " . $_POST['gitcom'] . "'" . $com . "git push -u origin master" . $push;
-    } else if ($_POST["gitmaster"] == "false") {
+        echo "3 -> git commit -am '" . $_POST['gitcom'] . "' " . $com . " git push -u origin master " . $push;
+    } else {
 
         $com = shell_exec("git commit -am " . $_POST['gitcom']);
         $push = shell_exec("git push -u" . $_POST['gitto']);
-        echo "4 -> git commit -am 'self commit " . $_POST['gitcom'] . "'" . $com . "git push -u" . $_POST['gitto']  . $push;
+        echo "4 -> git commit -am '" . $_POST['gitcom'] . "' " . $com . " git push -u " . $_POST['gitto']  . " " . $push;
     }
 }
---
+++ SDK/php/hash-pass.php
@@ -0,0 +1,9 @@
+<?php
+
+if (isset($_POST["pass"])) {
+    $password = $_POST["pass"];
+    $options = [
+        'cost' => 13
+    ];
+    echo  $password . "   -->   " . password_hash($password, PASSWORD_ARGON2I, $options);
+}
