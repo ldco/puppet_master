54b2ec756fb92c6f90eef1554071bafff03416c2
ldco <ldco@pop-os.localdomain>
2020-05-06 06:01:31 +0300

    self commit 2020-05-06 06:01:30

--- SDK/sh/start--dev.sh
+++ SDK/sh/start--dev.sh
@@ -1,10 +1,10 @@
-#!/bin/bash 
-cd /mnt/sdb2/CURRENT_WORKING_DEV/f21app2020/root
-#read -p "enter number of localhost: " localhostN
-#gnome-terminal --tab -e "/bin/bash -c 'php -S localhost:800$localhostN'"
-#chromium-browser http://localhost:800$localhostN;
-#chromium-browser http://localhost:800$localhostN/SDK/index.php;
+#!/bin/bash
+read -p "enter number of localhost: " localhostN
+gnome-terminal --tab -e "/bin/bash -c 'php -S localhost:800$localhostN'"
+chromium http://localhost:800$localhostN;
+chromium http://localhost:800$localhostN/SDK/index.php;
 
-gnome-terminal --tab -e "/bin/bash -c 'php -S localhost:8000'"
-chromium-browser http://localhost:8000;
-chromium-browser http://localhost:8000/SDK/index.php;
+
+#fix for new system
+#echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
+g
