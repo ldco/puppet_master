bce0bfcdb0c67e22c3dd49d44e42e0655630e5ba
ldco <ldco@pop-os.localdomain>
2020-05-31 16:16:53 +0300

    self commit 2020-05-31 16:16:53

--- sys/modules/Blog.class.module.php
+++ sys/modules/Blog.class.module.php
@@ -13,23 +13,23 @@ class Blog
     public $title = null;
     public $table = "pm_blog";
     public $folder = "blog";
-    public function make($_id = null, $childClass = null, $class = "pm_blog")
+    public function __construct($_id = null, $childClass = null, $class = "pm_blog")
     {
         global $PM_PAGE_NUM;
         global $DB;
         if (!isset($DB)) $DB = new DB;
         echo '<div id="' . $_id . '" class="' . $class . '">';
-        $result = $DB->queryRaw("SELECT * FROM $this->table");
+        $result = $DB->queryRaw("SELECT * FROM $this->table ORDER BY blog_date DESC");
         if ($result) {
             while ($row = $result->fetch_assoc()) {
                 echo '<div class="' . $childClass . ' pm_blog_item">';
 
                 echo '<div class="pm_blog_name">' . $row["name_" . PM_LANG] . '</div>';
 
-                echo '<img src="' . PM_IMAGES_REL . 'page_' . $PM_PAGE_NUM . '/' . $this->folder . '/' . $row["img"] . '.png">';
+                echo '<img src="' . PM_IMAGES_REL . 'page_' . $PM_PAGE_NUM . '/' . $this->folder . '/' . $row["blog_img"] . '.png">';
 
                 echo '<div class="pm_blog_text">' . $row["text_" . PM_LANG] . '</div>';
-                echo '<div class="pm_blog_date">' . $row["date"] . '</div>';
+                echo '<div class="pm_blog_date">' . $row["blog_date"] . '</div>';
             }
         }
         echo '</div>';
