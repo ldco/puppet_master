27248942dba046ebecc7bee10749b8178d9b19f5
ldco <ldco@pop-os.localdomain>
2021-02-06 23:58:11 +0200

    pages now in core

---
+++ core/Pages/1.page.php
@@ -0,0 +1,5 @@
+<?php
+
+declare(strict_types=1);
+require_once PM_SYS . "modules/requires_common.php";
+$page1 = new Page();
---
+++ core/Pages/2.page.php
@@ -0,0 +1,5 @@
+<?php
+
+declare(strict_types=1);
+require_once PM_SYS . "modules/requires_common.php";
+$page2 = new Page();
